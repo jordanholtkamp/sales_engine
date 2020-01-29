bdb55b471bab2b16ab5bd160b142d4f072364e11
Jordan Holtkamp <jordanholtkamp@gmail.com>
2020-01-29 09:20:40 -0700

    Move files around

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -2,6 +2,6 @@ class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
 
-  validates_presence_of :quantity
-  validates_presence_of :unit_price
+  # validates_presence_of :quantity
+  # validates_presence_of :unit_price
 end
