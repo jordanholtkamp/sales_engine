f531f64552d6eb4256b7b1bde3f66f57db370d28
Jordan Holtkamp <jordanholtkamp@gmail.com>
2020-01-29 22:25:38 -0700

    Item index and show - cannot figure out formatting for price

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -3,4 +3,9 @@ class ItemSerializer
 
   belongs_to :merchant
   attributes :id, :name, :description, :unit_price, :merchant_id
+
+  def unit_price
+    require 'pry'; binding.pry
+    unit_price / 100
+  end
 end
