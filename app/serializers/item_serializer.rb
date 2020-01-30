8853121fa9c00abd44aaac3f17b591875909d66c
Jordan Holtkamp <jordanholtkamp@gmail.com>
2020-01-29 22:43:07 -0700

    figure out attribute change in serializer

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -2,10 +2,9 @@ class ItemSerializer
   include FastJsonapi::ObjectSerializer
 
   belongs_to :merchant
-  attributes :id, :name, :description, :unit_price, :merchant_id
+  attributes :id, :name, :description, :merchant_id
 
-  def unit_price
-    require 'pry'; binding.pry
-    unit_price / 100
+  attribute :unit_price do |item|
+    item.unit_price / 100
   end
 end
